d83c91c49bb1c2b2e5feb3e693f6493cdd2cf7a9
guo <guoxy0119@163.com>
2024-06-04 09:34:40 +0800

    feat(链表)：简单题-160-相交链表

---
+++ src/com/atguigu/leetcode/linkedList/IntersectingLinkedList160.java
@@ -0,0 +1,62 @@
+package com.atguigu.leetcode.linkedList;
+
+/**
+ * 相交链表
+ * https://leetcode.cn/problems/intersection-of-two-linked-lists/solutions/10774/tu-jie-xiang-jiao-lian-biao-by-user7208t/
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ * 图示两个链表在节点 c1 开始相交：
+ * 题目数据 保证 整个链式结构中不存在环。
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * 自定义评测：
+ * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+ * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+ * listA - 第一个链表
+ * listB - 第二个链表
+ * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+ * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+ * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+ */
+public class IntersectingLinkedList160 {
+
+    public static void main(String[] args) {
+
+        ListNode listNode = new ListNode(8, new ListNode(4, new ListNode(5)));
+
+        ListNode headA = new ListNode(4, new ListNode(1, listNode));
+        ListNode headB = new ListNode(5, new ListNode(6, new ListNode(1, listNode)));
+
+        ListNode intersectionNode = getIntersectionNode(headA, headB);
+        System.out.println(intersectionNode.toString());
+
+    }
+
+
+    /**
+     * 根据题目意思
+     * 如果两个链表相交，那么相交点之后的长度是相同的
+     * <p>
+     * 我们需要做的事情是，让两个链表从同距离末尾同等距离的位置开始遍历。这个位置只能是较短链表的头结点位置。
+     * 为此，我们必须消除两个链表的长度差
+     * <p>
+     * 指针 pA 指向 A 链表，指针 pB 指向 B 链表，依次往后遍历
+     * 如果 pA 到了末尾，则 pA = headB 继续遍历
+     * 如果 pB 到了末尾，则 pB = headA 继续遍历
+     * 比较长的链表指针指向较短链表head时，长度差就消除了
+     * 如此，只需要将最短链表遍历两次即可找到位置
+     */
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode curA = headA;
+        ListNode curB = headB;
+
+        while (curA != curB) {
+            curA = curA == null ? headB : curA.next;
+            curB = curB == null ? headA : curB.next;
+        }
+
+        return curB;
+    }
+}
