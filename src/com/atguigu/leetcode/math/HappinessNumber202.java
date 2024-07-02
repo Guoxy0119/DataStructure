e2c617e407800ccfaadf670f8e086f5203bf1619
guo <guoxy0119@163.com>
2024-07-02 14:11:24 +0800

    feat(数学)：简单题-202. 快乐数（不快乐）

---
+++ src/com/atguigu/leetcode/math/HappinessNumber202.java
@@ -0,0 +1,67 @@
+package com.atguigu.leetcode.math;
+
+
+/**
+ * 202. 快乐数
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ * 「快乐数」 定义为：
+ * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ * 如果这个过程 结果为 1，那么这个数就是快乐数。
+ * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+ * <p>
+ * 示例 1：
+ * 输入：n = 19
+ * 输出：true
+ * 解释：
+ * 1^2 + 9^2 = 82
+ * 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100
+ * 1^2 + 0^2 + 02 = 1
+ * <p>
+ * 示例 2：
+ * 输入：n = 2
+ * 输出：false
+ */
+public class HappinessNumber202 {
+
+    public static void main(String[] args) {
+        int n = 116;
+
+        boolean happy = isHappy(n);
+        System.out.println(happy);
+    }
+
+    /**
+     * 官方答案：快慢指针
+     */
+    public static boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = getNext(n);
+        while (fast != 1 && slow != fast) {
+
+            slow = getNext(slow);
+            fast = getNext(getNext(fast)); // 不能只调用一次getNext，因为slow和fast的步长不一样
+
+        }
+
+        return fast == 1;
+
+    }
+
+
+    public static int getNext(int n) {
+
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10; //余数
+            n = n / 10;
+            totalSum += d * d;
+
+        }
+        return totalSum;
+
+    }
+
+}
