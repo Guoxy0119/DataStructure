626fcd760a09bf1357f42b96d35968f1b1db62f1
guo <guoxy0119@163.com>
2024-12-02 15:20:58 +0800

    feat(哈希)简单题-219-存在重复元素

--- src/com/atguigu/leetcode/array/easy/DuplicateElementExists219.java
+++ src/com/atguigu/leetcode/array/easy/DuplicateElementExists219.java
@@ -1,6 +1,6 @@
 package com.atguigu.leetcode.array.easy;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * 219. 存在重复元素 II
@@ -30,35 +30,32 @@ public class DuplicateElementExists219 {
         System.out.println(b);
     }
 
-
+    /**
+     * 思路
+     * 标签：哈希
+     * 维护一个哈希表，里面始终最多包含 k 个元素，当出现重复值时则说明在 k 距离内存在重复元素
+     * 每次遍历一个元素则将其加入哈希表中，如果哈希表的大小大于 k，则移除最前面的数字
+     * 时间复杂度：O(n)，n 为数组长度
+     * <p>
+     * 作者：画手大鹏
+     * 链接：https://leetcode.cn/problems/contains-duplicate-ii/solutions/13564/hua-jie-suan-fa-219-cun-zai-zhong-fu-yuan-su-ii-by/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
-        if (nums == null) {
-            return false;
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
         }
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        int index = 1;
-        for (int num : nums) {
-
-            Integer i = map.get(num);
-            System.out.println("master...");
-            System.out.println("dev-1...");
-            System.out.println("dev-1...");
-            System.out.println("dev-1...");
-            System.out.println("dev-1...");
-            System.out.println("dev-1...");
-            System.out.println("dev-1...");
-            System.out.println("dev-2...");
-            System.out.println("dev-2...");
-            System.out.println("dev-2...");
-            System.out.println("dev-2...");
-            System.out.println("dev-2...");
-
-
-        }
-
-        return true;
+        return false;
     }
+
 
 }
